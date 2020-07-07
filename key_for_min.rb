3856862adb5a6cca26251477fa522699b6f043db
congenial-course-2234 <jakelozano94@gmail.com>
2020-07-07 03:31:34 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,15 +5,15 @@
 def key_for_min_value(name_hash)
   lowest_number = nil
   answer = nil
-name_hash.each do |item, value|
+name_hash.each do |name, number|
   if lowest_number == nil
-    answer = item
-    lowest_number = value
+    answer = name
+    lowest_number = number
   end
-  if lowest_number > value
-    answer = item
-    lowest_number = value
+  if lowest_number > number
+    answer = name
+    lowest_number = number
+end
 end
 answer
-end
 end
